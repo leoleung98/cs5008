72eeefbffa2b575b0b5b3221951f3f7796fbd53b
jiahao liang <leoleung1998@login-students.ccs.neu.edu>
2022-05-20 19:00:00 -0400

    first commit

--- labassignment1/calculator.c
+++ labassignment1/calculator.c
@@ -1,5 +1,5 @@
-//enter your name here
-//enter your email here
+//Leo Liang
+//liang.jiahao@northeastern.edu
 #include<stdio.h>
 int main()
 {
@@ -12,7 +12,32 @@ int main()
     printf("Enter a and b values");
     scanf("%d %d", &a, &b);
     
-    
+    switch(choice){
+	case 1 :
+		printf("Addition\n");
+		c=a+b;
+		printf("Sum = %d\n",c);
+		break;
+	case 2 :
+		printf("Subtraction\n");
+		c=a-b;
+                printf("Sub = %d\n",c);
+                break;
+	case 3 :
+		printf("Multiplication\n");
+                c=a*b;
+                printf("Mul = %d\n",c);
+                break;
+	case 4 :
+		printf("Division\n");
+		if(b==0){
+			printf("Value for b can't be 0");
+			break;
+		}
+                c=a/b;
+                printf("Div = %d\n",c);
+                break;
+	}
            
     return 0;
 }
