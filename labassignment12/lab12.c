94e66a1ff2427c081e884fae9d59870c9eef7bef
leoleung98 <liang.jiahao@northeastern.edu>
2022-08-12 11:56:50 -0400

    compressor

--- labassignment12/lab12.c
+++ labassignment12/lab12.c
@@ -1,8 +1,8 @@
-//enter your email here
-//enter your name here
+// name: Leo Liang
+// email: liang.jiahao@northeastern.edu
 
 #include<stdio.h>
-#include<string.h>
+#include<string.h>     
 
 int compression(char arr[], int n, char res[]){
     int i;
@@ -12,17 +12,40 @@ int compression(char arr[], int n, char res[]){
     
     
     //insert your code here
-    
-    
-    
-    
-    
+    int j = 0;
+    if (n==1){
+        printf("%c\n",arr[0]);
+        return 1;
+    }
+    for (i = 1; i < n ; i++){
+        if (arr[i] == arr[index]) {
+            count++;
+        }
+        else {
+            res[++j] = arr[index];
+            printf("%c",res[j]);
+            if (count != 1) {
+                res[++j] = count;
+                printf("%d",res[j]);
+                count = 1;//aaaabcaa
+            }
+            index = i;
+        }
+    }
+    res[++j] = arr[index];
+    printf("%c",res[j]);
+    if (count != 1) {
+        res[++j] = count;
+        printf("%d",res[j]);
+    }
+    printf("\n");
+    return j ;
 }
  
 int main()
 {
-    char a[]="aaaaaaaaaaaaaabbbbcccd";
-    char res[50];
+    char a[]="aaaaaaaaaaaaaabbbbcccd";//
+    char res[50];//a14b4c3d
     int r,n=strlen(a);//n is the size of input array
     r=compression(a,n,res);
     printf("length of the compressed string:%d\n",r);
